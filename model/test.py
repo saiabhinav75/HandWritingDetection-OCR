a4ba49fb948ddbd364b72582eb3c42b449b0d944
saiabhinav75 <abhinavabhi7575@gmail.com>
2023-10-05 14:21:31 +0530

    model

---
+++ model/test.py
@@ -0,0 +1,42 @@
+import cv2
+from easyocr import Reader
+
+font=cv2.FONT_HERSHEY_SIMPLEX
+fontscale=1
+color=(40, 100, 250)
+thickness=2
+
+from word_segmentation import thresholding,dilation,contours,segmenting
+img=cv2.imread('images\image3.jpg')
+
+langs=["en"]
+print("Languages to be parsed",langs)
+
+reader=Reader(langs,gpu=True)
+
+if __name__=="__main__":
+    img=cv2.imread("images\image3.jpg")
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    h,w, c=img.shape
+
+    if w > 1000:
+        new_w=1000
+        ar=w/h
+        new_h=int(new_w/ar)
+        img=cv2.resize(img,(new_w,new_h),interpolation=cv2.INTER_AREA)
+
+    thresh_img=thresholding(img.copy())
+    dilated=dilation(thresh_img.copy())
+    contour_lines=contours(dilated.copy())
+    listOfWords=segmenting(img,contour_lines)
+
+for i in range(len(listOfWords)):
+    roi = img[listOfWords[i][1]:listOfWords[i][3], listOfWords[i][0]:listOfWords[i][2]]
+    result=reader.readtext(roi)
+    if len(result):
+        word=result[0][1]
+        print(word)
+        x,y=listOfWords[i][0],listOfWords[i][1]
+        cv2.putText(img,word,(x-1,y-1),font,fontscale,color,thickness,cv2.LINE_AA)
+
+cv2.imwrite("output/image.png",img)
---
+++ model/word_segmentation.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np,matplotlib.pyplot as plt
+
+def thresholding(image):
+    img_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    ret,thresh=cv2.threshold(img_gray,80,255,cv2.THRESH_BINARY_INV)
+    # plt.imshow(thresh,cmap='gray')
+    return thresh
+
+
+def dilation(img):
+    kernel=np.ones((10,20),np.uint8)
+    dilated=cv2.dilate(img,kernel,iterations=1)
+    return dilated
+
+def contours(img):
+    (contours, heirarchy) = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    sorted_contours_lines = sorted(contours, key = lambda ctr : cv2.boundingRect(ctr)[1]) # (x, y, w, h)
+    return sorted_contours_lines
+
+def segmenting(img,contour_lines):
+    img2 = img.copy()
+    listOfWords=[]
+    for ctr in contour_lines:
+        
+        x,y,w,h = cv2.boundingRect(ctr)
+        cv2.rectangle(img2, (x,y), (x+w, y+h), (40, 100, 250), 2)
+        listOfWords.append([x,y,x+w,y+h])
+    return listOfWords
+
